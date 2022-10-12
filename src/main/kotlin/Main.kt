09d4d54397da240997dbb3d62ad03c09ced7d74a
Vitalii Novgorodov <fivcheg@gmail.com>
2022-10-12 22:07:30 +0300

    edit errors, add default value VK Pay

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,8 @@
 
 fun main(args: Array<String>) {
-    val amount = 100
+    val amount = 8000000
     val lastAmount = 5000;
-    val cardType = "Mir"
+    val cardType = "VK Pay"
     val commission: Double = userTypeCardDiscount(cardType, amount, lastAmount)
     var totalCommisson = if (cardType == "Visa" && commission == 0.0 || cardType == "Mir" && commission == 0.0) {35} else {amount * commission / 10000}
     println("$totalCommisson  рублей составляет комиссия")
@@ -11,8 +11,8 @@ fun userTypeCardDiscount (cardType: String, amount: Int, lastAmount: Int): Doubl
     val resultDiscount: Double = when (cardType){
         "Maestro" -> if (amount < 7500000 - lastAmount){0.0} else {0.6}
         "MasterCard" -> if (amount < 7500000 - lastAmount){0.0} else {0.6}
-        "Visa" -> if (amount > 3500){0.75} else {0.0}
-        "Mir" -> if (amount > 3500){0.75} else {0.0}
+        "Visa" -> if ((amount / 100 * 0.75) < 3500){0.0} else {0.75}
+        "Mir" -> if ((amount / 100 * 0.75) < 3500){0.0} else {0.75}
         "VK Pay" -> 0.0
         else -> 0.0
     }
